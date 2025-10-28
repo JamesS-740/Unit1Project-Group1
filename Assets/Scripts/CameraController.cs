8271af260888165428e4f46c0d9f0b15b10bdf68
Daviruss67 <mruss3010@outlook.com>
2025-10-28 16:21:11 +0000

    Added Rotation around the player
    I added rotation around the player with the user controlling the camera movement with the mosue

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,26 +2,36 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // Getting Components from Unity
     public GameObject player;
-    private Vector3 offset;
+    public Rigidbody rb;
     public Transform target;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Declaring Variables
+    private Vector3 offset;
+    public float sens = 60;
+
     void Start()
     {
+        // Get the distance for the camera to stay at
         offset = transform.position - player.transform.position;
+
+        rb = GetComponent<Rigidbody>();
+
+        //Locks the cursor to the play area and makes it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        transform.position = player.transform.position + offset;
-    }
 
     private void Update()
     {
+        // Gets the mouse input from the user
         float mouseX = Input.GetAxisRaw("Mouse X");
 
-       
+        // Rotates the camera around an empty gameObject placed in the player around the Y-axis
+        // The user controlls this rotation by moving the mouse left and right 
+        transform.RotateAround(target.position, Vector3.up * mouseX * sens, Time.deltaTime * 100);
     }
 }
+
