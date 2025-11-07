f3ed773b3a66a2cad0a13ec2cc0e92d352b947ea
Daviruss67 <mruss3010@outlook.com>
2025-11-07 11:32:10 +0000

    Comments

--- Assets/Scripts/pickup.cs
+++ Assets/Scripts/pickup.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 public class pickup : MonoBehaviour
 {
     float Rotatespeed = 3;
-
     float time;
+    
     private void OnTriggerEnter(Collider other)
     {
         if(other.transform.tag == "Player")
         {
+            //Once the player touches the pickup, the game object is deactivated
             gameObject.SetActive(false);
         }
     }
 
     private void Update()
     {
+        //rotates the collectible around the y-axis 
         transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime * Rotatespeed);
     }
 
