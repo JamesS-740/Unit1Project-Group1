fae957e0a00df7f887b4fb2ff83bd9ffb916dfd2
Daviruss67 <mruss3010@outlook.com>
2025-11-07 11:18:17 +0000

    UI
    Added text

--- Assets/Scripts/Collisions.cs
+++ Assets/Scripts/Collisions.cs
@@ -1,8 +1,24 @@
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using static UnityEngine.SceneManagement.SceneManager;
+using TMPro;
 
 public class CollisionScript : MonoBehaviour
 {
+
+    int score = 0;
+    public TextMeshProUGUI text;
+
+    private void Start()
+    {
+        countScore();
+    }
+
+    private void Update()
+    {
+        countScore();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "spike")
@@ -11,5 +27,19 @@ public class CollisionScript : MonoBehaviour
             LoadScene(GetActiveScene().buildIndex);
         }
     }
-}
 
+    private void OnTriggerEnter(Collider other)
+    {
+        //Checks if the collider the player passes through has the tag pickup then adds one to the variable 'score'
+        if (other.transform.tag == "Pickup")
+        {
+            score = score + 1;
+            Debug.Log(score);
+        }
+    }
+
+    void countScore()
+    {
+        text.text = "Score: " + score + "/25";
+    }
+}
