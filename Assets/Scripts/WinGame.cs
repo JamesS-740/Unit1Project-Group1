260201eeaa16b4d04300187470f7ce8d85aa94bf
Daviruss67 <mruss3010@outlook.com>
2025-11-07 11:30:28 +0000

    Comments

--- Assets/Scripts/WinGame.cs
+++ Assets/Scripts/WinGame.cs
@@ -4,19 +4,24 @@ public class FinishGame : MonoBehaviour
 {
     public GameObject player;
 
-    PlayerMovement score;
+    //calls the Collisions
+    Collisions score;
+
+    //Keeps the variable private while allowing us the edit how many points are required to finish the game, we can do this in the inspector
     [SerializeField] int scoreRequirement = 2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        score = player.GetComponent<PlayerMovement>();
+        //Makes the variable score have the same variables as the public Collision variables
+        score = player.GetComponent<Collisions>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player" && score.score >= scoreRequirement)
         {
+            //if the player has the correct amount of points and they hit the collider, they win the game
             Debug.Log("You win");
         }
     }
